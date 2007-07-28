68611709304ce500085515fa7cbfaf68ab094776
SND\bdesmet_cp <SND\bdesmet_cp@81e6e737-5a20-4d0f-a97c-cb01297b46e0>
2007-07-28 23:59:16 +0000

    Orcas Beta 2 compliant implementation of LINQSQO.
    git-svn-id: https://linqsqo.svn.codeplex.com/svn@25120 81e6e737-5a20-4d0f-a97c-cb01297b46e0

---
+++ August07 (Orcas beta 2)/Query/MatchIt/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MatchIt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var res1 = from mi in typeof(System.Linq.Enumerable).GetMethods() orderby mi.Name group mi by mi.Name into g select new { Name = g.Key, Overloads = g.Count() };
+            foreach (var mi in res1)
+                Console.WriteLine("{0} has {1} overloads", mi.Name, mi.Overloads);
+
+            var res2 = from mi in typeof(BdsSoft.Linq.Enumerable).GetMethods() orderby mi.Name group mi by mi.Name into g select new { Name = g.Key, Overloads = g.Count() };
+            foreach (var mi in res2)
+                Console.WriteLine("{0} has {1} overloads", mi.Name, mi.Overloads);
+
+            //Console.WriteLine(res1.SequenceEqual(res2));
+
+            var mismatches = from m1 in res1
+                             join m2 in res2 on m1.Name equals m2.Name
+                             where m1.Overloads != m2.Overloads
+                             select new { m1.Name, Theirs = m1.Overloads, Ours = m2.Overloads };
+
+            foreach (var m in mismatches)
+                Console.WriteLine("{0} has {1} overloads instead of {2}", m.Name, m.Ours, m.Theirs);
+        }
+    }
+}
